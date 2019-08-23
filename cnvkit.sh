12ccaddc26e7e83eb6520beaf5c27ecaa92fddcf
goldrieve <45576436+goldrieve@users.noreply.github.com>
2019-08-23 15:39:12 +0100

    Update cnvkit.sh

--- cnvkit.sh
+++ cnvkit.sh
@@ -1,12 +1,3 @@
 source activate main_env
 
-cnvkit.py batch \
-	--method wgs \
-	~/TERGO/NEK_assemblies/927_aln/Normal/*.bam \		
-	--normal ~/TERGO/NEK_assemblies/927_aln/NEK_START_sorted.bam \
-	--fasta ~/TERGO/Reference_genomes/TriTrypDB-44_TbruceiTREU927_Genome.fasta \
-	--output-dir ~/TERGO/NEK_assemblies/ \
-	--scatter \
-	--diagram \
-	--target-avg-size 200 \
-	-p
+cnvkit.py batch --method wgs ~/TERGO/NEK_assemblies/927_aln/*.bam -n --target-avg-size 1000 -p --fasta ~/TERGO/Reference_genomes/TriTrypDB-44_TbruceiTREU927_Genome.fasta --output-dir ~/TERGO/NEK_assemblies/CNV_kit/1000_bin --scatter --diagram
