caa05e5a829a4c7ce2a10670d8b8e43a72ed7c9c
goldrieve <45576436+goldrieve@users.noreply.github.com>
2019-09-11 08:18:46 +0100

    Update Chromosome_coverage_position.R

--- Chromosome_coverage_position.R
+++ Chromosome_coverage_position.R
@@ -1,6 +1,3 @@
-#This script uses gene counts to create chromsome wide coverage plots of each gene. Each gene is split into one of 11
-#chromosomes and a line is plotted for each sample. 
-
 library(ggplot2)
 library(ggpubr)
 library(ggrepel)
@@ -10,8 +7,8 @@ library(dplyr)
 
 DNA <- read.csv(file.choose())
 
-DNA_int <- DNA [3:11]
-DNA_Melt = melt(DNA_int, measure.vars = c("END","START","TbruceiTREU927","Clone_2","Clone_3","Clone_4","Clone_5"),
+DNA_int <- DNA [2:11]
+DNA_Melt = melt(DNA_int, measure.vars = c("NEK_START","NEK_END"),
                 variable.name = "Cell_line", value.name = "Count")
 
 DNA_Melt$Count[DNA_Melt$Count > 10] <- NA #Removed all values above 10to enable chromsome wide changes
@@ -19,147 +16,159 @@ DNA_Melt$Count[DNA_Melt$Count > 10] <- NA #Removed all values above 10to enable 
 cell_line <- (as.character(DNA_Melt$Cell_line))
 out <- split(DNA_Melt , f = DNA_Melt$chr)
 
-C1 <- ggplot(data = out$Tb927_01_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+Genome <- ggplot(data = DNA_Melt, mapping = aes(x=Length/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
+  scale_y_continuous(limits=c(0, 10)) +
+  geom_line(aes(color=Cell_line, group = Cell_line)) +
+  ggtitle("") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_hline(yintercept=1, linetype="dashed",
+             color = "black", size=1)
+
+C1 <- ggplot(data = out$Tb927_01_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_01_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
-ggplot(data = out$Tb927_02_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C2 <- ggplot(data = out$Tb927_02_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
-  scale_x_discrete(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_02_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C3 <- ggplot(data = out$Tb927_03_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C3 <- ggplot(data = out$Tb927_03_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_03_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C4 <- ggplot(data = out$Tb927_04_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C4 <- ggplot(data = out$Tb927_04_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_04_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C5 <- ggplot(data = out$Tb927_05_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") + 
+C5 <- ggplot(data = out$Tb927_05_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_05_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C6 <- ggplot(data = out$Tb927_06_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C6 <- ggplot(data = out$Tb927_06_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_06_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C7 <- ggplot(data = out$Tb927_07_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C7 <- ggplot(data = out$Tb927_07_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_07_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C8 <- ggplot(data = out$Tb927_08_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C8 <- ggplot(data = out$Tb927_08_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_08_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C9 <- ggplot(data = out$Tb927_09_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C9 <- ggplot(data = out$Tb927_09_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_09_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C10 <- ggplot(data = out$Tb927_10_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C10 <- ggplot(data = out$Tb927_10_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_10_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-C11 <- ggplot(data = out$Tb927_11_v5.1, mapping = aes(x=start, y=Count)) + 
-  theme(legend.position="") + 
-  ylab("Relative read depth") + 
-  xlab("Genomic position (bp)") +
+C11 <- ggplot(data = out$Tb927_11_v5.1, mapping = aes(x=start/1000, y=Count)) +
+  theme(legend.position="") +
+  ylab("Relative read depth") +
+  xlab("Genomic position (kbp)") +
   scale_y_continuous(limits=c(0, 10)) +
   geom_line(aes(color=Cell_line, group = Cell_line)) +
   ggtitle("Tb927_11_v5.1") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept=1, linetype="dashed", 
+  geom_hline(yintercept=1, linetype="dashed",
              color = "black", size=1)
 
 
-ggarrange(C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, nrow = 6, ncol = 2, common.legend = TRUE)
+ggarrange(C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, nrow = 4, ncol = 3, common.legend = TRUE)
+
