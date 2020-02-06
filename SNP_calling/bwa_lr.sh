33ac1e2c1b709c8c611b43edaf1d45d7edf3eb19
goldrieve <45576436+goldrieve@users.noreply.github.com>
2020-02-06 17:03:12 +0000

    Jan

--- SNP_calling/bwa_lr.sh
+++ SNP_calling/bwa_lr.sh
@@ -1,13 +1,13 @@
 source activate main_env
 
-for sample in `ls /Users/s1886853/ncbi/public/sra/Fastq/*_1.fastq.gz`
+for sample in `ls /Users/s1886853/ncbi/public/sra/Fastq/Single/*.fastq.gz`
 
 do
 
-dir="/Users/s1886853/ncbi/public/sra/Fastq"
-base=$(basename $sample "_1.fastq.gz")
+dir="/Users/s1886853/ncbi/public/sra/Fastq/Single"
+base=$(basename $sample ".fastq.gz")
 
-bwa mem -M -t 14 -R "@RG\tID:${base}\tSM:${base}\tLB:${base}" /Users/s1886853/TERGO/Reference_genomes/Tb927/TriTrypDB-46_TbruceiTREU927_Genome.fasta ${dir}/${base}_1.fastq.gz ${dir}/${base}_2.fastq.gz | samtools view -hbS - | samtools sort -m 1000000000 > /Users/s1886853/TERGO/Monomorph/Tcomp/Trypanozoon/${base}_sorted.bam
+bwa mem -M -t 14 -R "@RG\tID:${base}\tSM:${base}\tLB:${base}" /Users/s1886853/TERGO/Reference_genomes/Tb927/TriTrypDB-46_TbruceiTREU927_Genome.fasta ${dir}/${base}.fastq.gz | samtools view -hbS - | samtools sort -m 1000000000 > /Users/s1886853/TERGO/Monomorph/Tcomp/Trypanozoon/${base}_sorted.bam
 
 done
 
--- SNP_calling/mark_dup.index.sh
+++
@@ -1,8 +0,0 @@
-for sample in `ls /Users/s1886853/TERGO/SIF_pathway/Alignments/*.marked.bam`
-do
-dir="/Users/s1886853/TERGO/SIF_pathway/Alignments"
-base=$(basename $sample ".marked.bam")
-
-picard BuildBamIndex I=${dir}/${base}.marked.bam
-
-done
